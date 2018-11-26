799e04671c0e8725a245590d966dccd4ba07735f
Olaoluwa3 <olaoko3@gmail.com>
2018-11-26 09:59:04 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Song {
+    private String FirstName;
+    private String LastName;
+    private int SSN;
+    private String DOB;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
